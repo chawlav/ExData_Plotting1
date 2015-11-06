1148345e2985ef513a0926f97ae8e0600623128e
Vishal Chawla <vchawla@Vishals-MacBook-Air.local>
2015-11-06 14:12:00 -0800

    plot2 minor changes

--- plot2.R
+++ plot2.R
@@ -1,20 +1,3 @@
-# plot1 <- function(){
-# 	#read the data
-# 	df <- read.csv("household_power_consumption.txt",header = TRUE, sep = ";", stringsAsFactors=FALSE)
-# 	#format the Date column(2) as a Date (read in as a string)
-# 	df$Date <- as.Date(df$Date, format="%d/%m/%Y")
-# 	#prune the data to only contain rows of interest (between 2007-02-01 and 2007-02-02) 
-# 	df <- df[df$Date >= as.Date("2007-02-01") & df$Date <= as.Date("2007-02-02"), ]
-# 	#coerce the column of interest to numeric
-# 	df$Global_active_power = as.numeric(df$Global_active_power)
-# 	hist(df$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
-# 	dev.copy(png, file = "plot1.png")
-# 	dev.off()
-
-
-# }
-
-
 plot2 <- function(){
 	#read the data
 	df <- read.csv("household_power_consumption.txt",header = TRUE, sep = ";", stringsAsFactors=FALSE)
@@ -31,7 +14,6 @@ plot2 <- function(){
 	plot(df$timestamp,df$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)",xlab="")
 	dev.copy(png, file = "plot2.png")
 	dev.off()
-
 
 }
 
